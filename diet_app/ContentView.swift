dc5d5899bdf9d8ff5bd65216efeade69ab88d909
Karol <karol.korzen@icloud.com>
2020-06-02 01:06:31 +0200

    Initial Commit

---
+++ diet_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  diet_app
+//
+//  Created by Karol Korzeń on 02/06/2020.
+//  Copyright © 2020 Karol Korzeń. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
